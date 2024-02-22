fdd6034ced774036be869d43ac22f42f63797c84
adityaKumari31 <aditya.kumari@pw.live>
2024-02-22 17:00:10 +0530

    chore(PUA-2962): Added event tracker

--- src/widgets/BatchDescription/BatchDetailsSchedule.tsx
+++ src/widgets/BatchDescription/BatchDetailsSchedule.tsx
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-import {ISubject} from "@/api/interfaces/batch";
-import {cn, getFullName, imageToImageUrl} from "@/lib/utils";
-import {Button} from "@/components/ui/button";
-import formatDate from "@/lib/date.utils";
-import {useState} from "react";
-import BatchDescriptionCardWrapper from "./BatchDescriptionCardWrapper";
+import { ISubject } from '@/api/interfaces/batch';
+import { cn, getFullName, imageToImageUrl } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import formatDate from '@/lib/date.utils';
+import { useState } from 'react';
 import batchEventTracker from "@/lib/BatchEventTracker/BatchEventTracker";
 import { useRouter } from "next/router";
+import BatchDescriptionCardWrapper from './BatchDescriptionCardWrapper';
 
 export default function BatchDetailsSchedule({subjects, batch_name, batch_id, batch_price}: { subjects: ISubject[], batch_name: string, batch_id: string, batch_price:{amount: number,
     discount: number,
@@ -15,68 +14,11 @@ export default function BatchDetailsSchedule({subjects, batch_name, batch_id, ba
     const [showMore, setShowMore] = useState(false);
     const router= useRouter();
 const getClassAndExam = router.asPath.split('/')
-    const handleDownload = (subject: any) =>{
-        batchEventTracker.scheduleDownLoad(batch_name, batch_price.amount, batch_price.total, batch_id,(getClassAndExam[2]? getClassAndExam[2] :""), (getClassAndExam[3]?getClassAndExam[3].split('?')[0] : ''))
-        const pdfUrl = imageToImageUrl(subject.fileId);
-        window.open(pdfUrl, '_blank');
-    }
-    return <BatchDescriptionCardWrapper title={'Batch Schedules'}>
-
-        <>
-            {
-                (showMore ? subjects : subjects.slice(0, 3)).map((subject, index) => {
-                    const schedule = subject.schedules[0]
-                    const teachersName = subject?.teacherIds
-                        ?.map((teacher: any) => {
-                            return getFullName(teacher);
-                        })
-                        .join(' & ');
-                    return <div key={index}
-                                className={cn('rounded-lg p-4 animationFromBottom !slide-in-from-bottom-3', {
-                                    'bg-[#EFF8FF]': index % 3 === 0,
-                                    'bg-[#F4F3FF]': index % 3 === 1,
-                                    'bg-[#FEFBE8]': index % 3 === 2,
-                                })}>
-                        <div className={'flex flex-col md:flex-row items-start md:items-center gap-2.5'}>
-                            <div
-                                className={cn('flex-1 py-1 h-full flex flex-col relative justify-between pl-3', {})}>
-                                <div className={cn('w-[4px] absolute rounded-md -left-[2px] top-0 bottom-0', {
-                                    'bg-[#2E90FA]': index % 3 === 0,
-                                    'bg-[#7A5AF8]': index % 3 === 1,
-                                    'bg-[#EAAA08]': index % 3 === 2,
-                                })}/>
-                                <div className={'text-xl font-semibold '}>
-                                    {subject.subject}
-                                </div>
-                                <div className={'flex md:items-center flex-col md:flex-row items-start mt-1'}>
-                                    <div
-                                        className={'text-sm text-lighter font-medium md:border-r md:pr-3 whitespace-nowrap'}> {formatDate(schedule.startTime)} to {formatDate(schedule.endTime)}</div>
-                                    <div
-                                        className={'text-sm text-lighter font-medium md:border-r md:pl-3 md:pr-3'}> {subject.lectureCount} Lectures
-                                    </div>
-                                    <div
-                                        className={'text-sm text-lighter font-medium md:pl-3 '}> {teachersName}</div>
-                                </div>
-                            </div>
-                            {imageToImageUrl(subject.fileId) &&
-                                <div className={'flex flex-col items-center justify-center'}>
-                                    <Button className={'bg-zinc-800'} onClick={() => {handleDownload(subject)
-                                    }}>
-                                        Download
-                                    </Button>
-                                </div>
-                            }
-                        </div>
-=======
-import { ISubject } from '@/api/interfaces/batch';
-import { cn, getFullName, imageToImageUrl } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import formatDate from '@/lib/date.utils';
-import { useState } from 'react';
-import BatchDescriptionCardWrapper from './BatchDescriptionCardWrapper';
-
-export default function BatchDetailsSchedule({ subjects }: { subjects: ISubject[] }) {
-  const [showMore, setShowMore] = useState(false);
+const handleDownload = (subject: any) =>{
+    batchEventTracker.scheduleDownLoad(batch_name, batch_price.amount, batch_price.total, batch_id,(getClassAndExam[2]? getClassAndExam[2] :""), (getClassAndExam[3]?getClassAndExam[3].split('?')[0] : ''))
+    const pdfUrl = imageToImageUrl(subject.fileId);
+    window.open(pdfUrl, '_blank');
+}
   return <BatchDescriptionCardWrapper title={'Batch Schedules'}>
 
     <>
@@ -111,7 +53,6 @@ export default function BatchDetailsSchedule({ subjects }: { subjects: ISubject[
                     <div
                       className={'text-sm text-lighter font-medium md:border-r md:pr-3 whitespace-nowrap h-full'}>
                       {formatDate(schedule.startTime)} to {formatDate(schedule.endTime)}
->>>>>>> 077dcf31ca2308810f1a00cd1ed7ff1c0ddf9535
                     </div>
                   }
                   <div
@@ -126,10 +67,7 @@ export default function BatchDetailsSchedule({ subjects }: { subjects: ISubject[
               </div>
               {imageToImageUrl(subject.fileId) &&
                 <div className={'flex flex-col items-center justify-center'}>
-                  <Button className={'bg-zinc-800'} onClick={() => {
-                    const pdfUrl = imageToImageUrl(subject.fileId);
-                    window.open(pdfUrl, '_blank');
-                  }}>
+                  <Button className={'bg-zinc-800'} onClick={() => {handleDownload(subject) }}>
                     Download
                   </Button>
                 </div>
