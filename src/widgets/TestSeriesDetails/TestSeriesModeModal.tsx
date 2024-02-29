8cea3a31743abf8475dbb919cf81ece8bf64535d
amandeep.singh <Amandeep.singh@pw.live>
2024-02-29 12:10:32 +0530

    feat(PUA-2296): dialouge box , modal in mweb

--- src/widgets/TestSeriesDetails/TestSeriesModeModal.tsx
+++ src/widgets/TestSeriesDetails/TestSeriesModeModal.tsx
@@ -38,25 +38,27 @@ export default function TestSeriesModeModal({trigger}: TestSeriesModeModalProps)
                 trigger
             }
         </DialogTrigger>
-        <DialogContent className="w-[700px]">
+        <DialogContent className="max-w-[720px]">
             <DialogHeader >
-                <DialogTitle className={'text-xl border-b pb-2'}>Test Series Mode</DialogTitle>
+                <DialogTitle className={'text-base sm:text-xl border-b text-left sm:text-center pb-2'}>Test Series Mode</DialogTitle>
                 <DialogDescription className={'pt-2 flex flex-col gap-4'}>
-                    <h4 className={'font-medium text-normal'}>Select one of the options to continue:</h4>
-                    <div className="flex gap-2">
+                    <h4 className={'font-medium text-sm text-left sm:text-center sm:text-normal'}>Select one of the options to continue:</h4>
+                    <div className="sm:flex-row flex flex-col gap-2">
                         {
                             Mode.map((m, index) => {
-                                return  <div key={index} onClick={()=>setSelectedMode(index)} className={`${selectedMode===index? 'bg-[#ECEAFF] border-[#5A4BDA]':' border-[#EFEFEF]'} rounded-lg flex flex-col py-8 px-4 items-center mb-6 cursor-pointer border`}>
-                                    <Image src={TestModalIcon.src} className="w-[160px] h-[103px]" />
-                                    <label htmlFor={m.title} className={'text-lg text-[#1B2124] font-bold mt-7 mb-1'}>{m.title}</label>
-                                    <p className={'text-[#3D3D3D] text-center text-[12px] leading-[18px] capitalize mb-2'}>{m.description}</p>
-                                    <p className={` ${selectedMode === index? 'text-[#5A4BDA]':'text-[#1B2124]'} font-semibold text-2xl`}>{m.price}</p>
+                                return <div key={index} onClick={()=>setSelectedMode(index)} className={`${selectedMode===index? 'bg-[#ECEAFF] border-[#5A4BDA]':' border-[#EFEFEF]'} rounded-lg flex flex-row-reverse sm:flex-col sm:py-8 py-4 px-4 items-center sm:mb-6 cursor-pointer border`}>
+                                    <Image src={TestModalIcon.src} className="sm:w-[160px] sm:h-[103px] w-[114px] h-[73px] bg-center bg-cover " />
+                                    <div className="text-left sm:text-center gap-1.5 flex flex-col sm:mt-7 justify-between w-[90%] sm:w-full">
+                                    <label htmlFor={m.title} className={'sm:text-lg text-base text-[#1B2124] font-semibold sm:font-bold sm:mb-1'}>{m.title}</label>
+                                    <p className={'text-[#3D3D3D] text-[12px] leading-[18px] capitalize sm:mb-2 '}>{m.description}</p>
+                                    <p className={` ${selectedMode === index? 'text-[#5A4BDA]':'text-[#1B2124]'} font-semibold text-lg sm:text-2xl`}>{m.price}</p>
+                                    </div>
                                 </div>
                             })
                         }
                     </div>
-                    <div className="flex justify-end">
-                    <Button className="py-4 px-12">Continue</Button>
+                    <div className="flex justify-end sm:grid">
+                    <Button className="py-4 px-12 w-full ">Continue</Button>
                     </div>
                </DialogDescription>
             </DialogHeader>
