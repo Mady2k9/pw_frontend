68b2afd53396571fd8600dcff94e102e307778ff
Reddemonfox <him.sharma.98@gmail.com>
2024-03-02 20:09:53 +0530

    Implement Google Tag Manager and update environment variables
    Added Google Tag Manager scripts to _document.tsx for better tracking and updated environment variables in .env file to align with new staging settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,10 +37,29 @@ export default function Document() {
           dangerouslySetInnerHTML={SoftwareAppSchema()}
           key="SoftwareAppSchema"
         />
-        {/*<script dangerouslySetInnerHTML={GoogleTagManager()} />*/}
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TGJHCW7D');`}
+        </script>
       </Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-THMQFCKGHP" />
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
+          gtag('config', 'G-THMQFCKGHP');`}
+      </script>
       <body>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGJHCW7D"
+                height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+      </noscript>
+
+
       <Main />
       <NextScript />
       {/*<noscript*/}
