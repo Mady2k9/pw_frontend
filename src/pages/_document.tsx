b81b71637c6cf671ffe8a5f16f65d0061e1c6b7e
Reddemonfox <him.sharma.98@gmail.com>
2024-03-02 22:28:27 +0530

    Modify Google Tag Manager script integration and update ID
    The Google Tag Manager integration script was restructured to offer better readability and modularization. The refactoring also included an update to the Google Tag Manager ID in the _document.tsx file. Consequently, the changes enhance the integration of the GTM script and 'noscript' fallback in the application's HTML structure.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,13 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document';
 import SoftwareAppSchema from '@/deprecated/schema/SoftwareAppSchema';
 import OrganisationAppSchema from '@/deprecated/schema/OrganisationAppSchema';
 
+const GTM_ID = 'GTM-TGJHCW7D';
+
 function GoogleTagManager() {
   return {
-    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TGJHCW7D');`,
+    __html: ` window.addEventListener('DOMContentLoaded', function() {
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          });`,
   };
 }
 
