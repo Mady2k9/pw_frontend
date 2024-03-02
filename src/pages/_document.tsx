aa39dcceec52c27dcdaeedacda6bde12f69f729a
Reddemonfox <him.sharma.98@gmail.com>
2024-03-02 20:39:59 +0530

    Update Google Tag Manager integration in _document.tsx
    Revised the Google Tag Manager script to a simpler, more manageable form and moved it from the head of the _document.tsx file. Additionally, this update required adjustments to the positioning of `Main` and `NextScript` components within the body. These updates ensure cleaner, more efficient document structure.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,15 +37,18 @@ export default function Document() {
           dangerouslySetInnerHTML={SoftwareAppSchema()}
           key="SoftwareAppSchema"
         />
-        <script>
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TGJHCW7D');`}
-        </script>
       </Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-THMQFCKGHP" />
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-THMQFCKGHP');`}
+      </script>
       <body>
+
+
       <Main />
       <NextScript />
       <noscript>
