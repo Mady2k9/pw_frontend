e4e9bbe95e6865633fa994f888968a81d04e4551
Reddemonfox <him.sharma.98@gmail.com>
2024-03-02 21:50:19 +0530

    Update Google Tag Manager ID and refactor script integration
    Changed the Google Tag Manager (GTM) ID and refactored the integration code in _document.tsx to use helper functions. This approach improves readability and modularity of GTM script and 'noscript' fallback inclusion in our HTML structure.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,14 +8,14 @@ function GoogleTagManager() {
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TLKGKK9');`,
+  })(window,document,'script','dataLayer','GTM-TGJHCW7D');`,
   };
 }
 
 function GTMNoScript() {
   return {
     __html: `<iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-TLKGKK9"
+        src="https://www.googletagmanager.com/ns.html?id=GTM-TGJHCW7D"
         height="0"
         width="0"
         style="display:none;visibility:hidden"
@@ -27,16 +27,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script>
-          {
-            `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TGJHCW7D');`
-          }
-        </script>
-
+        <script dangerouslySetInnerHTML={GoogleTagManager()} />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={OrganisationAppSchema()}
@@ -55,10 +46,9 @@ export default function Document() {
       <Main />
       <NextScript />
       </body>
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGJHCW7D"
-                height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
-      </noscript>
+      <noscript
+        dangerouslySetInnerHTML={GTMNoScript()}
+      />
     </Html>
   );
 }
