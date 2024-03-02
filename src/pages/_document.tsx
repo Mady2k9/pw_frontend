0e0c401ea1bc36423ff66f2de1ded42198eeb9f2
Reddemonfox <him.sharma.98@gmail.com>
2024-03-02 21:22:46 +0530

    Update Google Tag Manager integration in _document.tsx
    The previous Google Tag Manager script has been replaced with a new one in the _document.tsx page. Also, a 'noscript' fall-back iframe intended for users with JavaScript disabled was added to ensure continuous data measurement and analysis.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -27,6 +27,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script>
+          {
+            `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TGJHCW7D');`
+          }
+        </script>
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={OrganisationAppSchema()}
@@ -38,24 +48,17 @@ export default function Document() {
           key="SoftwareAppSchema"
         />
       </Head>
-      {/*<script async src="https://www.googletagmanager.com/gtag/js?id=G-THMQFCKGHP" />*/}
-      {/*<script>*/}
-      {/*  {`window.dataLayer = window.dataLayer || [];*/}
-      {/*    function gtag(){dataLayer.push(arguments);}*/}
-      {/*    gtag('js', new Date());*/}
 
-      {/*    gtag('config', 'G-THMQFCKGHP');`}*/}
-      {/*</script>*/}
       <body>
 
 
       <Main />
       <NextScript />
-      {/*<noscript>*/}
-      {/*  <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGJHCW7D"*/}
-      {/*          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>*/}
-      {/*</noscript>*/}
       </body>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGJHCW7D"
+                height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+      </noscript>
     </Html>
   );
 }
