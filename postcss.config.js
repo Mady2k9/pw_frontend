77028ecb5a4f2de2d7064e04dba3002ba1b1337b
Reddemonfox <him.sharma.98@gmail.com>
2024-03-04 10:27:00 +0530

    Add PostCSS Flexbugs Fixes and PostCSS Preset Env to dependencies
    The update includes the addition of "postcss-flexbugs-fixes" and "postcss-preset-env" to the project's dependencies. Also, the "postcss-purgecss" was added to the devDependencies. It ensures better usability and functionality for the project.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,29 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {}
-  },
+  plugins: [
+    // restore the Next.js default behavior
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+    [
+      // configure PurgeCSS
+      "@fullhuman/postcss-purgecss",
+      {
+        content: ["./src/**/*.{js,jsx,ts,tsx}"],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: {
+          standard: ["html", "body"],
+        },
+      },
+    ],
+  ],
 };
