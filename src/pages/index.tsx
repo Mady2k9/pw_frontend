5d45d076fdbc9d3d89d2b4e9b31e6b436a5a15d6
Reddemonfox <him.sharma.98@gmail.com>
2024-03-05 21:54:35 +0530

    Comment out server-side props and home functions in index.tsx
    The functions getServerSideProps and Home inside index.tsx have been commented out entirely. This possibly suggests preparations for a code rewrite or significant modifications. The exact reason for this change might need further investigation.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,104 +24,104 @@ import { ExamCategoryProps } from '@/widgets/ExamCategorySection/ExamCategoryCar
 import { IWidgetJson } from '@/api/interfaces/page';
 
 
-export async function getServerSideProps() {
-  let HomePageData;
-  let headerData;
-  let footerData;
-
-  try {
-    const result = await Promise.all([
-      FetchHomePage(),
-      FetchHeader(),
-      FetchFooter(),
-    ]);
-    HomePageData = result[0];
-    headerData = result[1];
-    footerData = result[2];
-  } catch (error) {
-    // console.log(error);
-  }
-  // if (!headerData?.data || !footerData?.data) {
-  //   console.log('Redirecting to home page');
-  //   return {
-  //     redirect: {
-  //       permanent: false,
-  //       destination: '/',
-  //     },
-  //     props: {},
-  //   };
-  // }
-  return {
-    props: {
-      HomePageData: HomePageData || {},
-      headerData: headerData?.data || {},
-      footerData: footerData || {},
-    },
-  };
-}
+// export async function getServerSideProps() {
+//   let HomePageData;
+//   let headerData;
+//   let footerData;
+//
+//   try {
+//     const result = await Promise.all([
+//       FetchHomePage(),
+//       FetchHeader(),
+//       FetchFooter(),
+//     ]);
+//     HomePageData = result[0];
+//     headerData = result[1];
+//     footerData = result[2];
+//   } catch (error) {
+//     // console.log(error);
+//   }
+//   // if (!headerData?.data || !footerData?.data) {
+//   //   console.log('Redirecting to home page');
+//   //   return {
+//   //     redirect: {
+//   //       permanent: false,
+//   //       destination: '/',
+//   //     },
+//   //     props: {},
+//   //   };
+//   // }
+//   return {
+//     props: {
+//       HomePageData: HomePageData || {},
+//       headerData: headerData?.data || {},
+//       footerData: footerData || {},
+//     },
+//   };
+// }
 function Home(props: any) {
-  const pageData = useMemo(()=>{
-    return props?.HomePageData?.data?.widgetJson
-  },[props?.HomePageData?.data?.widgetJson]);
-  const router = useRouter();
-  useEffect(() => {
-    verifyToken().then((resp) => {
-      if (resp == 200) router.push('/study');
-    });
-  }, [router]);
-  useEffect(() => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const marketingCampaigns = {} as any;
-    const setMarketingAttribute = (paramName: string) => {
-      const paramValue = queryParams.get(paramName);
-      if (paramValue) {
-        marketingCampaigns[paramName] = paramValue;
-      }
-    };
-    setMarketingAttribute('utm_source');
-    setMarketingAttribute('utm_medium');
-    setMarketingAttribute('utm_campaign');
-    setMarketingAttribute('utm_term');
-    setMarketingAttribute('utm_content');
-    setMarketingAttribute('gclid');
-    setMarketingAttribute('fbclid');
-    setMarketingAttribute('igshid');
-    if (Object.keys(marketingCampaigns).length > 0) {
-      const jsonMarketingCampaign = JSON.stringify(marketingCampaigns);
-      localStorage.setItem('campaignParam', btoa(jsonMarketingCampaign));
-    }
-  });
-
-  useEffect(() => {
-    eventTracker.pwLandingPage();
-  }, []);
-
-  const widgetData = useMemo(() => {
-    const x = pageData?.[WidgetEnum.EXAM_CATEGORIES];
-    const categories: ExamCategoryProps[] = [];
-    x?.sectionProps?.map((category: IWidgetJson) => {
-      const categoryData: any = category;
-      categories.push({
-        name: categoryData['categoryName'],
-        icon: categoryData['icon'],
-        color: categoryData?.cta?.['backGroundColor'],
-        slug: categoryData?.cta?.['ctaRedirectionUrl'],
-        actionName: categoryData?.cta?.['text'],
-        actionColor: categoryData?.cta?.['textColor'],
-        exams: categoryData?.options?.map((option: any) => {
-          return {
-            slug: option.redirectionUrl,
-            name: option.className,
-          };
-        }) || [],
-        displayOrder: 0
-      });
-    });
-    return {
-      ...x,
-      categories: categories,
-    };
-  }, [pageData]);
+  // const pageData = useMemo(()=>{
+  //   return props?.HomePageData?.data?.widgetJson
+  // },[props?.HomePageData?.data?.widgetJson]);
+  // const router = useRouter();
+  // useEffect(() => {
+  //   verifyToken().then((resp) => {
+  //     if (resp == 200) router.push('/study');
+  //   });
+  // }, [router]);
+  // useEffect(() => {
+  //   const queryParams = new URLSearchParams(window.location.search);
+  //   const marketingCampaigns = {} as any;
+  //   const setMarketingAttribute = (paramName: string) => {
+  //     const paramValue = queryParams.get(paramName);
+  //     if (paramValue) {
+  //       marketingCampaigns[paramName] = paramValue;
+  //     }
+  //   };
+  //   setMarketingAttribute('utm_source');
+  //   setMarketingAttribute('utm_medium');
+  //   setMarketingAttribute('utm_campaign');
+  //   setMarketingAttribute('utm_term');
+  //   setMarketingAttribute('utm_content');
+  //   setMarketingAttribute('gclid');
+  //   setMarketingAttribute('fbclid');
+  //   setMarketingAttribute('igshid');
+  //   if (Object.keys(marketingCampaigns).length > 0) {
+  //     const jsonMarketingCampaign = JSON.stringify(marketingCampaigns);
+  //     localStorage.setItem('campaignParam', btoa(jsonMarketingCampaign));
+  //   }
+  // });
+  //
+  // useEffect(() => {
+  //   eventTracker.pwLandingPage();
+  // }, []);
+  //
+  // const widgetData = useMemo(() => {
+  //   const x = pageData?.[WidgetEnum.EXAM_CATEGORIES];
+  //   const categories: ExamCategoryProps[] = [];
+  //   x?.sectionProps?.map((category: IWidgetJson) => {
+  //     const categoryData: any = category;
+  //     categories.push({
+  //       name: categoryData['categoryName'],
+  //       icon: categoryData['icon'],
+  //       color: categoryData?.cta?.['backGroundColor'],
+  //       slug: categoryData?.cta?.['ctaRedirectionUrl'],
+  //       actionName: categoryData?.cta?.['text'],
+  //       actionColor: categoryData?.cta?.['textColor'],
+  //       exams: categoryData?.options?.map((option: any) => {
+  //         return {
+  //           slug: option.redirectionUrl,
+  //           name: option.className,
+  //         };
+  //       }) || [],
+  //       displayOrder: 0
+  //     });
+  //   });
+  //   return {
+  //     ...x,
+  //     categories: categories,
+  //   };
+  // }, [pageData]);
   return (
     <div>
      PW Live
