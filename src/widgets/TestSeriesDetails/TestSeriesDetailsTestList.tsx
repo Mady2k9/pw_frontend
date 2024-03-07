9e4cf1810b6b15227822908806ca5d454993e70a
amandeep.singh <Amandeep.singh@pw.live>
2024-03-07 21:09:49 +0530

    fix(staging) : Fix currency issue

--- src/widgets/TestSeriesDetails/TestSeriesDetailsTestList.tsx
+++ src/widgets/TestSeriesDetails/TestSeriesDetailsTestList.tsx
@@ -9,18 +9,17 @@ export default function TestSeriesDetailsTestList({testData}:{testData:any}) {
 
     const [showAll, setShowAll] = useState(false);
     console.log(testData,'testData');
-    
     return <div className={'card-shadow rounded-lg p-4 flex flex-col gap-4 md:gap-6'} id={'Tests-List'}>
         <h2 className={'text-xl md:text-[32px] leading-[48px] font-bold'}>Tests</h2>
         <div className={'flex flex-col gap-4'}>
             {
-                (showAll ? testData?.data : testData?.data?.splice(0, 2))?.map((test:any, index:number) => {
+                (showAll ? testData : testData?.splice(0, 2))?.map((test:any, index:number) => {
                     return <TestCard key={index} {...test}/>
                 })
             }
         </div>
         {
-            testData?.data?.length <= 2 ? null :
+            testData?.length <= 2 ? null :
                 <Button variant={'secondary'} onClick={() => {
                     scrollToElement(document.getElementById('Tests-List')!);
                     setShowAll(!showAll);
@@ -29,7 +28,7 @@ export default function TestSeriesDetailsTestList({testData}:{testData:any}) {
                     className={cn('w-5 h-5 stroke-primary mr-2', {
                         'rotate-180': showAll
                     })}/> View Less Tests
-                    ({testData?.data?.length})</Button>
+                    ({testData?.length})</Button>
         }
     </div>;
 }
