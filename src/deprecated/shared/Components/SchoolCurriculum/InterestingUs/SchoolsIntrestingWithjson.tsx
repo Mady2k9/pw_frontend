e615f559ebcea16ef351a0467539aec922511c17
Madhuresh <mady2k9@gmail.com>
2024-03-08 23:35:30 +0530

    feat(PUA-2970): Pw schools all  data Changes

--- src/deprecated/shared/Components/SchoolCurriculum/InterestingUs/SchoolsIntrestingWithjson.tsx
+++ src/deprecated/shared/Components/SchoolCurriculum/InterestingUs/SchoolsIntrestingWithjson.tsx
@@ -9,29 +9,29 @@ export const IntrestingData = {
     {
       image: `${MultidisciplinaryApproach.src}`,      
       heading: "Multidisciplinary Approach",
-      subheding: "Making education holistic by developing human capabilities through arts and crafts, music and movement"
+      subheding: "Making education holistic by developing human capabilities through arts and crafts, music and movement."
     },
     {
       image: `${PlayActivityBasedLearning.src}`,      
       heading: "Play & Activity Based Learning",
-      subheding: "Emphasis on play and activity-based learning to stimulate children’s imagination, creativity, and problem-solving skills"
+      subheding: "Emphasis on play and activity-based learning to stimulate children’s imagination, creativity, and problem-solving skills."
     },
     {
       image: `${Technologicalntegration.src}`,      
       heading: "Technological Integration",
-      subheding: "Digital tools like multimedia videos, online quizzes, animation rhymes, and other digital resources to create a robust curriculum"
+      subheding: "Digital tools like multimedia videos, online quizzes, animation rhymes, and other digital resources to create a robust curriculum."
 
     },
     {
       image: `${ExperentialLearning.src}`,      
       heading: "Experiential Learning",
-      subheding: "Exploratory activities, role-play exercises, and a story-telling-based approach to foster a deeper comprehension of concepts"
+      subheding: "Exploratory activities, role-play exercises, and a story-telling-based approach to foster a deeper comprehension of concepts."
 
     },
     {
       image: `${Analyticalskills.src}`,      
       heading: "Analytical Skills & Creative Thinking",
-      subheding: "Logical decision-making and innovation are encouraged through mazes, games, riddles, puzzles, and critical thinking questions"
+      subheding: "Logical decision-making and innovation are encouraged through mazes, games, riddles, puzzles, and critical thinking questions."
 
     },
     {
